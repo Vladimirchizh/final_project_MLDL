aba0f977e8da2eee4da65cfa0d14833d16519cf8
Vladimir <vc@inbox-marketing.ru>
2020-11-29 19:26:06 +0100

    adding hourly volume

--- nn.py
+++ nn.py
@@ -4,4 +4,7 @@ dtc_data = pd.read_csv(r'Radar_Traffic_Counts.csv')
 #%%
 dtc_data['location_name'] = dtc_data \
     .location_name.apply(lambda x: x.strip()) 
- 
+# %%
+hourly_vol = dtc_data.groupby(['location_name','location_latitude','location_longitude','Direction','Hour']).agg({'Volume':'mean'}).reset_index()
+hourly_vol
+# %% 
